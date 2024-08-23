cbff924b7a3e1a841b66a916f0f95f6bf846836d
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-08-23 17:53:28 +0300

    [20] [Time Beats: 40.75%] [Memory Beats: 48.56%] - LeetPush

--- 20-Valid-Parentheses.cpp
+++ 20-Valid-Parentheses.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='('||s[i]=='['||s[i]=='{')
-            {
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='['||s[i]=='{'||s[i]=='('){
                 st.push(s[i]);
             }
-            else
-            {
-                if(st.empty()||    (s[i] == ')' && st.top() != '(') || 
-                    (s[i] == '}' && st.top() != '{') ||
-                    (s[i] == ']' && st.top() != '['))
-                    {
-                        return false;
-                    }
-               st.pop();     
+            else if (st.empty()||s[i]==']'&&st.top()!='['||s[i]=='}'&&st.top()!='{'||s[i]==')'&&st.top()!='('){
+                return  false;
+            }
+            else{
+                st.pop();
             }
         }
-        return st.empty();
+    return st.empty();
     }
 };
