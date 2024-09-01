dad54e6e2cb9385215a18f0ae58698f90d7eb84b
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-09-01 14:53:19 +0300

    [53] [Time Beats: 74.03%] [Memory Beats: 5.05%] - LeetPush

---
+++ 53-Maximum-Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int> nums) {
+    int n = nums.size();
+    int curSum = 0, res = INT_MIN;
+    int L = 0;
+    for (int i = 0; i < n; i++) {
+        curSum += nums[i];
+        if (i > 0 && (curSum<curSum - nums[L] || nums[i]>curSum)) {
+            while (curSum < nums[i]) {
+                curSum -= nums[L];
+                L++;
+            }
+        }
+        res = max(res, curSum);
+    }
+    return res;
+}
+};
