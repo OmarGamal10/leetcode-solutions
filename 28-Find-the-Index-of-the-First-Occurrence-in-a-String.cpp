38f9f964dbab026ca3c28ef5fde7f12a8a42e02b
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-09-01 14:15:53 +0300

    [28] [Time Beats: 100.00%] [Memory Beats: 80.32%] - LeetPush

---
+++ 28-Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n=haystack.size();
+        int m=needle.size();
+        for(int i=0;i<n-m+1;++i){
+            for(int j=0;j<m;++j){
+                if(needle[j]!=haystack[i+j])
+                    break;
+                if(j==m-1)
+                    return i;    
+            }
+        }
+        return -1;
+    }
+};
