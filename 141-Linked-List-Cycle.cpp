25e79a86d26b70f79c159fbaaf3fd4d471e3f86b
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-09-07 13:47:07 +0300

    [141] [Time Beats: 85.02%] [Memory Beats: 21.69%] - LeetPush

---
+++ 141-Linked-List-Cycle.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+  struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode*s=head,*f=head;
+        while(f&&f->next){
+            s=s->next;
+            f=f->next->next;
+            if(s==f)
+                return true;
+        }
+        return false;
+    }
+};
