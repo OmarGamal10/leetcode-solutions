203b98142f85aeea37a5c9c2c5343c53b3d309bb
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-09-13 15:46:53 +0300

    [189] [Time Beats: 68.63%] [Memory Beats: 92.74%] - LeetPush

---
+++ 189-Rotate-Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size=nums.size();
+        k%=size;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
+    }
+};
