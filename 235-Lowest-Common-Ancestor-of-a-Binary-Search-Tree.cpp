4d83cc00b62c8528d68df339fa6819e744fdbf0a
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-09-19 22:09:20 +0300

    [235] [Time Beats: 65.16%] [Memory Beats: 86.70%] - LeetPush

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
@@ -10,11 +10,11 @@
 
 class Solution {
 public:
+    //this is for general binary trees, can be optimized bcz question wants a bst
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root)
             return nullptr;
-        if((p->val>root->val&&q->val<root->val)||(p->val<root->val&&q->val>root->val))
-            return root;
+        
         TreeNode*left=lowestCommonAncestor(root->left,p,q);
         TreeNode*right=lowestCommonAncestor(root->right,p,q);
         if(root==p||root==q)
