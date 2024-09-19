ef79f93abe882fbc0e6ba72cfc735d5645d1580e
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-09-19 22:05:59 +0300

    [235] [Time Beats: 84.63%] [Memory Beats: 86.70%] - LeetPush

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
@@ -13,6 +13,8 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root)
             return nullptr;
+        if((p->val>root->val&&q->val<root->val)||(p->val<root->val&&q->val>root->val))
+            return root;
         TreeNode*left=lowestCommonAncestor(root->left,p,q);
         TreeNode*right=lowestCommonAncestor(root->right,p,q);
         if(root==p||root==q)
