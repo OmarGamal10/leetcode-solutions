1fa7110af83efb1186b809c4dbb599966a564090
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-10-16 15:13:07 +0300

    [80] [Time Beats: 30.65%] [Memory Beats: 88.94%] - LeetPush

---
+++ 80-Remove-Duplicates-from-Sorted-Array-II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<3)
+            return nums.size();
+       int j=2;
+       for(int i=2;i<nums.size();++i){
+            if(nums[i]!=nums[j-2]){
+                nums[j]=nums[i];
+                j++;
+            }
+       }
+       return j;
+    }
+};
