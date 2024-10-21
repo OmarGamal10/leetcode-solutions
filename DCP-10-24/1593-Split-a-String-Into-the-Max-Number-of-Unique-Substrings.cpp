169c514cf18180f0275e3fa7c31f49a72d9842f0
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-10-21 22:22:42 +0300

    [1593] [Time Beats: 26.32%] [Memory Beats: 35.09%] - LeetPush

---
+++ DCP-10-24/1593-Split-a-String-Into-the-Max-Number-of-Unique-Substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void split(string&s,unordered_set<string>&curr,int i,int &ans){
+        if(i==s.size())
+        {
+            ans=max((int)curr.size(),ans);
+            return;
+        }
+        for(int j=i;j<=s.size();++j){
+            string sub=s.substr(i,j-i+1);
+            if(curr.count(sub))
+                continue;
+            else{
+                curr.insert(sub);
+                split(s,curr,j+1,ans);
+                curr.erase(sub);
+                
+            }
+        }
+    }
+
+    int maxUniqueSplit(string s) {
+        unordered_set<string>curr;
+        int ans=0;
+        split(s,curr,0,ans);
+        return ans;
+    }
+};
