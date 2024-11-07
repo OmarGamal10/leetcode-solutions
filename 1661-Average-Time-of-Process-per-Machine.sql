accf757ba83be95c6efb890a908555df06f2375f
OmarGamal10 <146660720+OmarGamal10@users.noreply.github.com>
2024-11-08 01:27:54 +0200

    [1661] [Time Beats: 49.54%] - LeetPush

--- 1661-Average-Time-of-Process-per-Machine.sql
+++ 1661-Average-Time-of-Process-per-Machine.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select machine_id,Round(AVG
 (CASE WHEN activity_type='end' then timestamp
- when activity_type='start' then -timestamp end)*2,3) as processing_time
+ else -timestamp end)*2,3) as processing_time
 from Activity
 group by machine_id;
